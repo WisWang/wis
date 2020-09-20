8fd7b75ad1b46b06d8b7883fb1fdee622c049454
hongzhi.wang <hongzhi.wang@moji.com>
2020-09-20 10:31:04 +0800

    wis-2020-09-20

--- homework/k8s.sh
+++ homework/k8s.sh
@@ -2,5 +2,6 @@
 k label node kind-control-plane ingress-ready=true
 k apply -f kubernetes/
 
-echo "$(k describe node |grep InternalIP |awk '{print $NF}') www.wis.com static.wis.com" >> /etc/hosts
+echo "$(k describe node |grep InternalIP |awk '{print $NF}') www.dev.wis.com static.dev.wis.com" >> /etc/hosts
+echo "$(k describe node |grep InternalIP |awk '{print $NF}') www.test.wis.com static.test.wis.com" >> /etc/hosts
 
